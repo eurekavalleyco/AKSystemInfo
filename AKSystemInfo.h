22850f5063c1cae2bdf5273c3c47d0446a80f2ec
Ken M. Haggerty <eurekavalleyco@gmail.com>
2015-06-29 11:13:27 -0400

    Adds Internet status methods

--- AKSystemInfo.h
+++ AKSystemInfo.h
@@ -1,6 +1,6 @@
 //
 //  AKSystemInfo.h
-//  AKSuperViewController
+//  AKSystemInfo
 //
 //  Created by Ken M. Haggerty on 11/11/13.
 //  Copyright (c) 2013 Eureka Valley Co. All rights reserved.
@@ -17,6 +17,14 @@
 
 #pragma mark - // DEFINITIONS (Public) //
 
+#define NOTIFICATION_INTERNETSTATUS_DID_CHANGE @"kNotificationInternetStatusDidChange"
+
+typedef enum {
+    AKDisconnected = 0,
+    AKConnectedViaWWAN,
+    AKConnectedViaWiFi
+} AKInternetStatus;
+
 @interface AKSystemInfo : NSObject
 + (float)iOSVersion;
 + (CGSize)screenSize;
@@ -29,4 +37,7 @@
 + (void)setStatusBarStyle:(UIStatusBarStyle)style animated:(BOOL)animated;
 + (UIColor *)iOSBlue;
 + (BOOL)viewIsUsingAutoLayout:(UIView *)view;
++ (BOOL)isReachable;
++ (BOOL)isReachableViaWiFi;
++ (BOOL)isReachableViaWWAN;
 @end
--- AKSystemInfo.m
+++ AKSystemInfo.m
@@ -1,6 +1,6 @@
 //
 //  AKSystemInfo.m
-//  AKSuperViewController
+//  AKSystemInfo
 //
 //  Created by Ken M. Haggerty on 11/11/13.
 //  Copyright (c) 2013 Eureka Valley Co. All rights reserved.
@@ -12,14 +12,66 @@
 
 #import "AKSystemInfo.h"
 #import "AKDebugger.h"
+#import "AKGenerics.h"
+#import "Reachability.h"
+#import "AKPrivateInfo.h"
 
 #pragma mark - // DEFINITIONS (Private) //
+
+@interface AKSystemInfo ()
+@property (nonatomic, strong) Reachability *reachability;
++ (id)sharedInfo;
++ (Reachability *)reachability;
+- (void)setup;
+- (void)teardown;
+- (void)internetStatusDidChange:(NSNotification *)notification;
+@end
 
 @implementation AKSystemInfo
 
 #pragma mark - // SETTERS AND GETTERS //
 
+- (Reachability *)reachability
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeGetter customCategories:nil message:nil];
+    
+    if (_reachability) return _reachability;
+    
+    _reachability = [Reachability reachabilityWithHostName:[AKPrivateInfo reachabilityDomain]];
+    [_reachability startNotifier];
+    return _reachability;
+}
+
 #pragma mark - // INITS AND LOADS //
+
+- (id)init
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup customCategories:nil message:nil];
+    
+    self = [super init];
+    if (!self)
+    {
+        [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeCritical methodType:AKMethodTypeSetup customCategories:nil message:[NSString stringWithFormat:@"Could not initialize %@", stringFromVariable(self)]];
+        return nil;
+    }
+    [self setup];
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup customCategories:nil message:nil];
+    
+    [super awakeFromNib];
+    [self setup];
+}
+
+- (void)dealloc
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup customCategories:nil message:nil];
+    
+    [self teardown];
+}
 
 #pragma mark - // PUBLIC METHODS //
 
@@ -107,10 +159,84 @@
     else return NO;
 }
 
++ (BOOL)isReachable
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeGetter customCategories:nil message:nil];
+    
+    return [[[AKSystemInfo sharedInfo] reachability] isReachable];
+}
+
++ (BOOL)isReachableViaWiFi
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeGetter customCategories:nil message:nil];
+    
+    return [[[AKSystemInfo sharedInfo] reachability] isReachableViaWiFi];
+}
+
++ (BOOL)isReachableViaWWAN
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeGetter customCategories:nil message:nil];
+    
+    return [[[AKSystemInfo sharedInfo] reachability] isReachableViaWWAN];
+}
+
 #pragma mark - // DELEGATED METHODS //
 
 #pragma mark - // OVERWRITTEN METHODS //
 
 #pragma mark - // PRIVATE METHODS //
+
++ (id)sharedInfo
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup customCategories:nil message:nil];
+    
+    static AKSystemInfo *sharedInfo = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInfo = [[AKSystemInfo alloc] init];
+    });
+    return sharedInfo;
+}
+
++ (Reachability *)reachability
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeGetter customCategories:nil message:nil];
+    
+    return [[AKSystemInfo sharedInfo] reachability];
+}
+
+- (void)setup
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup customCategories:nil message:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(internetStatusDidChange:) name:kReachabilityChangedNotification object:self.reachability];
+}
+
+- (void)teardown
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup customCategories:nil message:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:kReachabilityChangedNotification object:self.reachability];
+}
+
+- (void)internetStatusDidChange:(NSNotification *)notification
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeUnspecified customCategories:@[AKD_NOTIFICATION_CENTER] message:nil];
+    
+    if (![AKSystemInfo isReachable])
+    {
+        [[NSNotificationCenter defaultCenter] postNotificationName:NOTIFICATION_INTERNETSTATUS_DID_CHANGE object:nil userInfo:[NSDictionary dictionaryWithObject:[NSNumber numberWithInteger:AKDisconnected] forKey:NOTIFICATION_OBJECT_KEY]];
+        return;
+    }
+    
+    if ([AKSystemInfo isReachableViaWiFi])
+    {
+        [[NSNotificationCenter defaultCenter] postNotificationName:NOTIFICATION_INTERNETSTATUS_DID_CHANGE object:nil userInfo:[NSDictionary dictionaryWithObject:[NSNumber numberWithInteger:AKConnectedViaWiFi] forKey:NOTIFICATION_OBJECT_KEY]];
+    }
+    if ([AKSystemInfo isReachableViaWWAN])
+    {
+        [[NSNotificationCenter defaultCenter] postNotificationName:NOTIFICATION_INTERNETSTATUS_DID_CHANGE object:nil userInfo:[NSDictionary dictionaryWithObject:[NSNumber numberWithInteger:AKConnectedViaWWAN] forKey:NOTIFICATION_OBJECT_KEY]];
+    }
+}
 
 @end
